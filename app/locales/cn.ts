4e5fcc98ac7c0192db51ca40cd90034cc88c46b4
qinghanpeter <60281440+qinghan1@users.noreply.github.com>
2023-06-18 12:41:28 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
+      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入你自己的 OpenAI API Key。\n关注微信公众号：清寒的小屋。\n聊天窗口发送“密码”，即可免费获取访问密码。",
   },
   Auth: {
     Title: "需要密码",
